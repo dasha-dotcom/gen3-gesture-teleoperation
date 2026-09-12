ad73ef9e8a11914fb7dbd9abfb1e6ceb862a9b3f
Dasha Nabokova <daryanabokova@icloud.com>
2026-09-12 12:23:14 -0400

    Set up ROS 2 Jazzy workspace and initial demo mode

---
+++ ros2_ws/src/hand_status_demo/hand_status_demo/hand_status_publisher.py
@@ -0,0 +1,30 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HandStatusPublisher(Node):
+	def __init__(self):
+		super().__init__('hand_status_publisher')
+		self.publisher_ = self.create_publisher(
+			String,
+			'hand_status',
+			10
+		)
+		self.timer = self.create_timer(1.0, self.publish_status)
+	def publish_status(self):
+		msg = String()
+		msg.data = 'HAND_VISIBLE'
+		self.publisher_.publish(msg)
+		self.get_logger().info(f'Publishing: {msg.data}')
+def main(args=None):
+		rclpy.init(args=args)
+
+		node = HandStatusPublisher()
+
+		rclpy.spin(node)
+
+		node.destroy_node()
+		rclpy.shutdown()
+
+if __name__ == '__main__':
+		main()
---
+++ ros2_ws/src/hand_status_demo/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'hand_status_demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dasha',
+    maintainer_email='dasha@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        	'hand_status_publisher = hand_status_demo.hand_status_publisher:main',
+    	],
+    },
+)
